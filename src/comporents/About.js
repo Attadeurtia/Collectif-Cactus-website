117e1f94bccd194aa6a29ea1fca045c881f30bae
attadeurtia <attadeurtia@hotmail.com>
2023-04-10 15:38:14 +0200

    update css

--- src/comporents/About.js
+++ src/comporents/About.js
@@ -1,7 +1,7 @@
 import Personnes from "./Personnes";
 
 export default function About(){
-    return <div className='App'>
+    return <div className='about'>
     <h2>À propos :</h2>
     <p>Le collectif Cactus est un projet vaste et encore indéfini nous unissant sous la bannière de ce concept, tenter, produite, expérimenter, apprendre. </p>
     <Personnes></Personnes>
--- src/comporents/ApplicationUnitaire.js
+++ src/comporents/ApplicationUnitaire.js
@@ -3,23 +3,23 @@ import { useState } from "react"
 export default function ApplicationUnitaire() {
 
     const [personnes] = useState([
-        { id: 1, nom: "Netflux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", photo: "https://avatars.githubusercontent.com/u/79169647?v=4" },
-        { id: 1, nom: "Filebrowser", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", photo: "https://www.apyart.com/3717-large_default/violet-fat-500ml.jpg" },
-        { id: 1, nom: "Blog", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", photo: "https://misterclick.files.wordpress.com/2008/03/cowboy-3-4.jpg" },
-        { id: 1, nom: "Jeux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", photo: "https://media.timeout.com/images/103671590/750/562/image.jpg" }
+        { id: 1, nom: "Netflux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://static-00.iconduck.com/assets.00/jellyfin-icon-512x512-jcuy5qbi.png" },
+        { id: 2, nom: "Filebrowser", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Nextcloud_Logo.svg/2560px-Nextcloud_Logo.svg.png" },
+        { id: 3, nom: "Blog", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://cdn-icons-png.flaticon.com/512/60/60736.png" },
+        { id: 4, nom: "Jeux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://e7.pngegg.com/pngimages/49/992/png-clipart-steam-logo-computer-icons-steam-tuners-steam-miscellaneous-rim.png" },
+        { id: 5, nom: "Atlas", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://w7.pngwing.com/pngs/208/848/png-transparent-musical-note-logo-clef-music-logo-design-other-text-musical-composition-thumbnail.png" },
+        { id: 6, nom: "Cinémans", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://tp4-cinemans.pages.dev/assets/logo/logo_white.svg" },
     ])
 
     return <div className='ApplicationUnitaire'>
 
         {personnes.map((personnes) => (
-            <button class="groupeApp" key={personnes.id}>
+            <button class="item" key={personnes.id}>
                 <img
-                    src={personnes.photo} >
+                    src={personnes.logo} alt="pdp">
                 </img>
-                <div className="texteApp">
-                    <h3>{personnes.nom}</h3>
-                    <p>{personnes.description}</p>
-                </div>
+                <h3>{personnes.nom}</h3>
+                <p>{personnes.description}</p>
             </button>
         ))}
     </div>
