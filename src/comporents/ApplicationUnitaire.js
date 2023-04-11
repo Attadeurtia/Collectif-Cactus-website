eeaf1384b3db13c63b152faad473d406aa87f3c6
attadeurtia <attadeurtia@hotmail.com>
2023-04-11 23:19:50 +0200

    refonte des bloques

--- src/comporents/ApplicationUnitaire.js
+++ src/comporents/ApplicationUnitaire.js
@@ -3,24 +3,24 @@ import { useState } from "react"
 export default function ApplicationUnitaire() {
 
     const [personnes] = useState([
-        { id: 1, nom: "Netflux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://static-00.iconduck.com/assets.00/jellyfin-icon-512x512-jcuy5qbi.png" },
-        { id: 2, nom: "Filebrowser", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Nextcloud_Logo.svg/2560px-Nextcloud_Logo.svg.png" },
-        { id: 3, nom: "Blog", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://cdn-icons-png.flaticon.com/512/60/60736.png" },
-        { id: 4, nom: "Jeux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://e7.pngegg.com/pngimages/49/992/png-clipart-steam-logo-computer-icons-steam-tuners-steam-miscellaneous-rim.png" },
-        { id: 5, nom: "Atlas", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://w7.pngwing.com/pngs/208/848/png-transparent-musical-note-logo-clef-music-logo-design-other-text-musical-composition-thumbnail.png" },
-        { id: 6, nom: "Cinémans", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir ", logo: "https://tp4-cinemans.pages.dev/assets/logo/logo_white.svg" },
+        { id: 1, nom: "Netflux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir", logo: "https://static-00.iconduck.com/assets.00/jellyfin-icon-512x512-jcuy5qbi.png" },
+        { id: 2, nom: "Filebrowser", description: "Un stockage de documents, gratuit, illimité et sécurisé", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Nextcloud_Logo.svg/2560px-Nextcloud_Logo.svg.png" },
+        { id: 3, nom: "Blog", description: "Nous découvrir à travers de nombreux sujets qui nous passionne, et que nous partageons", logo: "https://cdn-icons-png.flaticon.com/512/60/60736.png" },
+        { id: 4, nom: "Jeux", description: "Nos dernières créations indépendantes, disponible sur Steam !", logo: "https://e7.pngegg.com/pngimages/49/992/png-clipart-steam-logo-computer-icons-steam-tuners-steam-miscellaneous-rim.png" },
+        { id: 5, nom: "Atlas", description: "Nos créations musicales (MAO, groupe de musique ...) ", logo: "https://w7.pngwing.com/pngs/208/848/png-transparent-musical-note-logo-clef-music-logo-design-other-text-musical-composition-thumbnail.png" },
+        { id: 6, nom: "Cinémans", description: "À la manière d'AlloCiné ou SensCritique, notre site web dédié uniquement aux films et séries, à travers critiques, notes classement et bien plus encore", logo: "https://tp4-cinemans.pages.dev/assets/logo/logo_white.svg" },
     ])
 
     return <div className='ApplicationUnitaire'>
 
         {personnes.map((personnes) => (
-            <button class="itemApplication" key={personnes.id}>
+            <div class="itemApplication" key={personnes.id}>
                 <img
                     src={personnes.logo} alt="pdp">
                 </img>
                 <h3>{personnes.nom}</h3>
                 <p>{personnes.description}</p>
-            </button>
+            </div>
         ))}
     </div>
 }
--- src/comporents/Personnes.js
+++ src/comporents/Personnes.js
@@ -1,23 +1,23 @@
 import { useState } from "react"
+import logo from './personnes/Geoffrey.png';
 
 export default function Personnes() {
 
     const [personnes] = useState([
         { id: 1, nom: "Geoffrey", photo: "https://avatars.githubusercontent.com/u/79169647?v=4" },
         { id: 1, nom: "Julien", photo: "https://www.apyart.com/3717-large_default/violet-fat-500ml.jpg" },
-        { id: 1, nom: "Artur", photo: "../../src/assets/personnes/arturppv5.png" },
+        { id: 1, nom: "Artur", photo: "./arturppv5.png" },
         { id: 1, nom: "Amaury", photo: "../assets/personnes/Amaury.png" }
     ])
 
     return <div className='Personnes'>
-
         {personnes.map((personnes) => (
-            <h3 key={personnes.id}>
-                <img
-                    src={personnes.photo} >
-                </img>
-                {personnes.nom}
-            </h3>
+            <div class="itemPersonne" key={personnes.id}>
+                <img src={personnes.photo} alt="pdp" ></img>
+                <h3>{personnes.nom}</h3>
+                <img src="{`${process.env.PUBLIC_URL}/personnes/Amaury.png`} " alt="pdp" ></img>
+                
+            </div>
         ))}
     </div>
 }
