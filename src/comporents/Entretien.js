ae6e6e2e71122536b58a266f77c59c69e90c12bd
Attadeurtia <79169647+Attadeurtia@users.noreply.github.com>
2023-04-12 10:01:55 +0000

    rajout des images temporaires et centrage

--- src/comporents/Entretien.js
+++ src/comporents/Entretien.js
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 export default function Entretien() {
 
     return <div className='Entretien'>
--- src/comporents/Personnes.js
+++ src/comporents/Personnes.js
@@ -1,13 +1,12 @@
 import { useState } from "react"
-import logo from './personnes/Geoffrey.png';
 
 export default function Personnes() {
 
     const [personnes] = useState([
         { id: 1, nom: "Geoffrey", photo: "https://avatars.githubusercontent.com/u/79169647?v=4" },
-        { id: 1, nom: "Julien", photo: "https://www.apyart.com/3717-large_default/violet-fat-500ml.jpg" },
-        { id: 1, nom: "Artur", photo: "./arturppv5.png" },
-        { id: 1, nom: "Amaury", photo: "../assets/personnes/Amaury.png" }
+        { id: 1, nom: "Julien", photo: "https://github.com/Attadeurtia/Collectif-Cactus-website/blob/main/public/personnes/smallthief.png?raw=true" },
+        { id: 1, nom: "Artur", photo: "https://raw.githubusercontent.com/Attadeurtia/Collectif-Cactus-website/main/public/personnes/arturppv5.png" },
+        { id: 1, nom: "Amaury", photo: "https://github.com/Attadeurtia/Collectif-Cactus-website/blob/main/public/personnes/Amaury.png?raw=true" }
     ])
 
     return <div className='Personnes'>
@@ -15,8 +14,6 @@ export default function Personnes() {
             <div class="itemPersonne" key={personnes.id}>
                 <img src={personnes.photo} alt="pdp" ></img>
                 <h3>{personnes.nom}</h3>
-                <img src="{`${process.env.PUBLIC_URL}/personnes/Amaury.png`} " alt="pdp" ></img>
-                
             </div>
         ))}
     </div>
