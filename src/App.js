6aa964df0c77fe0875b63aefd703fd787025953c
Attadeurtia <Attadeurtia@hotmail.com>
2023-04-17 23:19:58 +0200

    rajout des cactus en fond

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import ApplicationSection from './comporents/ApplicationSection';
 import Entretien from './comporents/Entretien';
 import Hero from './comporents/Hero';
 
+import logso from './Union.png';
+
+
 function App() {
   return (
     <div className='App'>
+      <img class="union1" src={logso} alt="logo" />
+      <img class="union2" src={logso} alt="logo" />
+      <img class="union3" src={logso} alt="logo" />
+
       <Hero/>
       <About />
       <ApplicationSection/>
