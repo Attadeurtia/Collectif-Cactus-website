a5a690af6342bf69c368a45ac9fa0ddfdbe8b789
Julien Houget <julien.houget@sfr.fr>
2023-04-18 23:31:22 +0200

    Test cron 2.

--- src/comporents/Personnes.js
+++ src/comporents/Personnes.js
@@ -3,8 +3,8 @@ import { useState } from "react"
 export default function Personnes() {
 
     const [personnes] = useState([
-        { id: 1, nom: "Geoffrey le 2ème meilleur", photo: process.env.PUBLIC_URL + '/personnes/Geoffrey.png' },
-        { id: 1, nom: "Julien le meilleur", photo: process.env.PUBLIC_URL + '/personnes/Julien.png' },
+        { id: 1, nom: "Geoffrey", photo: process.env.PUBLIC_URL + '/personnes/Geoffrey.png' },
+        { id: 1, nom: "Julien", photo: process.env.PUBLIC_URL + '/personnes/Julien.png' },
         { id: 1, nom: "Artur", photo: process.env.PUBLIC_URL + '/personnes/Artur.png' },
         { id: 1, nom: "Amaury", photo: process.env.PUBLIC_URL + '/personnes/Amaury.png' }
     ])
