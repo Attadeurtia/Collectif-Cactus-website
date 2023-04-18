3cd9d534ecf1924623cc7214533162d51aac26a1
Julien Houget <julien.houget@sfr.fr>
2023-04-18 17:24:36 +0200

    Mets à jour l'image de Julien.

--- src/comporents/Personnes.js
+++ src/comporents/Personnes.js
@@ -4,7 +4,7 @@ export default function Personnes() {
 
     const [personnes] = useState([
         { id: 1, nom: "Geoffrey", photo: "https://avatars.githubusercontent.com/u/79169647?v=4" },
-        { id: 1, nom: "Julien", photo: "https://github.com/Attadeurtia/Collectif-Cactus-website/blob/main/public/personnes/smallthief.png?raw=true" },
+        { id: 1, nom: "Julien", photo: "https://github.com/Attadeurtia/Collectif-Cactus-website/blob/main/public/personnes/Julien.png?raw=true" },
         { id: 1, nom: "Artur", photo: "https://raw.githubusercontent.com/Attadeurtia/Collectif-Cactus-website/main/public/personnes/arturppv5.png" },
         { id: 1, nom: "Amaury", photo: "https://github.com/Attadeurtia/Collectif-Cactus-website/blob/main/public/personnes/Amaury.png?raw=true" }
     ])
