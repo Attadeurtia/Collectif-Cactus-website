9eae770bc03119588fafcb558c71c97582ccf811
attadeurtia <attadeurtia@hotmail.com>
2023-04-18 17:57:42 +0200

    boites a jour + changemnt nom de variable dans "ApplicationUnitaire"

--- src/comporents/ApplicationUnitaire.js
+++ src/comporents/ApplicationUnitaire.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function ApplicationUnitaire() {
 
-    const [personnes] = useState([
+    const [application] = useState([
         { id: 1, nom: "Netflux", description: "Notre sélection de film, musique et série, à découvrir ou faire découvrir.", logo: "https://static-00.iconduck.com/assets.00/jellyfin-icon-512x512-jcuy5qbi.png" },
         { id: 2, nom: "Filebrowser", description: "Un stockage de documents, gratuit, illimité et sécurisé.", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Nextcloud_Logo.svg/2560px-Nextcloud_Logo.svg.png" },
         { id: 3, nom: "Blog", description: "Nous découvrir à travers de nombreux sujets qui nous passionne, et que nous partageons.", logo: "https://cdn-icons-png.flaticon.com/512/60/60736.png" },
@@ -13,13 +13,13 @@ export default function ApplicationUnitaire() {
 
     return <div className='ApplicationUnitaire'>
 
-        {personnes.map((personnes) => (
-            <div class="itemApplication" key={personnes.id}>
+        {application.map((application) => (
+            <div class="itemApplication" key={application.id}>
                 <img
-                    src={personnes.logo} alt="pdp">
+                    src={application.logo} alt="pdp">
                 </img>
-                <h3>{personnes.nom}</h3>
-                <p>{personnes.description}</p>
+                <h3>{application.nom}</h3>
+                <p>{application.description}</p>
             </div>
         ))}
     </div>
