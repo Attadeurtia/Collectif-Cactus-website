0305f2226b0f16a09246a14fed2be7b6eb9333a9
Attadeurtia <Attadeurtia@hotmail.com>
2023-05-04 15:22:18 +0200

    produite -> produire

--- src/comporents/About.js
+++ src/comporents/About.js
@@ -3,7 +3,7 @@ import Personnes from "./Personnes";
 export default function About(){
     return <div className='about' >
     <h2 className='boite'>À propos :</h2>
-    <p className="boite">Le collectif Cactus est un projet vaste et encore indéfini nous unissant sous la bannière de ce concept, tenter, produite, expérimenter, apprendre. </p>
+    <p className="boite">Le collectif Cactus est un projet vaste et encore indéfini nous unissant sous la bannière de ce concept, tenter, produire, expérimenter, apprendre. </p>
     <Personnes></Personnes>
 </div>
 }
