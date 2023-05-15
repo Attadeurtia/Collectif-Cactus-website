bad54e1370c556ed2ad2be2a818b5353070a0cd7
Attadeurtia <Attadeurtia@hotmail.com>
2023-05-15 10:36:35 +0200

    responsive mobile réparer

--- src/comporents/About.js
+++ src/comporents/About.js
@@ -1,13 +1,9 @@
 import Personnes from "./Personnes";
-
-// quand on clique sur le bouton, on redirie vers une autre page
-
-
+  
 export default function About() {
     return <div className='about' >
         <h2 className='boite'>À propos :</h2>
-        <textarea className="boite">Le collectif Cactus est un projet vaste et encore indéfini nous unissant sous la bannière de ce concept, tenter, produire, expérimenter, apprendre. </textarea>
+        <textarea className="boite">Le collectif Cactus est un projet vaste et encore indéfini nous unissant sous la bannière de ce concept, tenter, produire, expérimenter, apprendre.</textarea>
         <Personnes></Personnes>
     </div>
 }
-
